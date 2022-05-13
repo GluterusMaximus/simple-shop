e229d096e9eef7ac0ccdcaccc69fddcacf66e517
GluterusMaximus <supkar1756@gmail.com>
2022-05-13 13:20:27 +0300

    User register

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,17 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers.js'
 import { cartReducer } from './reducers/cartReducers.js'
-import { userLoginReducer } from './reducers/userReducers.js'
+import {
+  userLoginReducer,
+  userRegisterReducer,
+} from './reducers/userReducers.js'
 
 const reducer = {
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 }
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
